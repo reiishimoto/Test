5b7a8c20e96ed5319b2f21f8ae65aaa22d6ff381
ootanitarou <knz2474525@stu.o-hara.ac.jp>
2025-04-24 11:53:31 +0900

    こここここ

--- src/chapter3/Hello.java
+++ src/chapter3/Hello.java
@@ -18,6 +18,8 @@ public class Hello extends HttpServlet {
 	) throws ServletException, IOException {
 		PrintWriter out=response.getWriter();
 		out.println("Hello!");
+		out.println("Hello!");
+		out.println("Hello!");
 		out.println(new java.util.Date());
 	}
 
